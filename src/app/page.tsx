ce1a7f8d9ca38c11d2623ff08ace0e4f15981766
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-10-16 22:21:56 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,111 @@
-import Image from "next/image";
+import { Raleway, Montserrat } from '@next/font/google';
 
-export default function Home() {
+const raleway = Raleway({ subsets: ['latin'] });
+const montserrat = Montserrat({ subsets: ['latin'] });
+
+export default function Responsive() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="maindiv">
+      <div className="header" role="navigation">
+        <a href="#default" className="logo" style={{ fontFamily: raleway.style.fontFamily }} aria-label="Digital Diaries Logo">Digital Diaries</a>
+        <div className="header-right">
+          <a className="active" href="#home" aria-current="page">Home</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </div>
+
+      <footer>
+        <div className="footer-content">
+          <h3 className="footer-title">Digital Diaries</h3>
+          <div className="footer-description">
+            <p >Welcome to Digital Diaries, where technology meets creativity! Dive into insightful articles, cutting-edge trends, <br />and expert guides designed to keep you ahead in the digital age. Whether you’re a tech novice or a seasoned <br /> pro, our content covers everything from coding tips to the latest in cybersecurity. Join our community of tech <br /> enthusiasts and embark on a journey through the fascinating world of IT.</p>
+
+          </div>
+          <div className="footer-description">
+
+            <p>Email: digitalDiaries@yahoo.com</p>
+            <p>Phone: (123) 456-7890</p>
+          </div>
+        </div>
       </footer>
+
+      <div className='parentContainer'>
+
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title">Tech Trends to Watch in 2024</h1>
+          <p className="description">
+            As we look ahead to 2024, several tech trends are set to shape the digital landscape.{" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title">The Role of AI in Enhancing Productivity</h1>
+          <p className="description">
+            Artificial Intelligence is revolutionizing how we work and live. {" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title"> 5 Must-Know Tips for Aspiring Web Developers</h1>
+          <p className="description">
+            Starting a career in web development can be daunting, but with the right tips, you can set yourself up for success!{" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title">Exploring the Intersection of Art and Technology</h1>
+          <p className="description">
+            Technology isn’t just about coding and cybersecurity; it also fuels creativity! {" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title">Cybersecurity Basics: Protecting Your Digital Life</h1>
+          <p className="description">
+            As technology advances, so do the threats lurking online. {" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+        <div className="childContainer bounce-in-right">
+          <div className="imageContainer">
+            <img src="path_to_image.jpg" alt="Description of the image" />
+          </div>
+
+          <h1 className="title">The Future of Coding: Embracing No-Code Platforms</h1>
+          <p className="description">
+            In today’s digital landscape, the need for coding skills is evolving{" "}
+          </p>
+          <button className="readMore">Read More</button>
+        </div>
+
+
+      </div>
     </div>
+
+
+    // {/* <div className="parentContainer">
+
+    // </div> */}
   );
 }
